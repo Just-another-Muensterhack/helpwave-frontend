abf3861091798adca18a355f10308f58adef35f6
Jannik Wibker <jannikwibker@gmail.com>
2022-09-23 16:37:11 +0200

    improved modal styles

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Pressable } from 'react-native'
 
 type ModalProps = {
   question: string,
@@ -9,13 +9,56 @@ type ModalProps = {
 
 const Modal: React.FC<ModalProps> = ({ question, answers, onAnswer }) => {
   return (
-    <View>
-      <Text>{question}</Text>
-      {answers.map((answer) => (
-        <Button key={answer} title={answer} onPress={() => onAnswer(answer)} />
-      ))}
+    <View style={styles.modal}>
+      <Text style={styles.question}>{question}</Text>
+      <View style={styles.answerContainer}>
+        {answers.map((answer) => (
+          <Pressable style={styles.answer} key={answer} onPress={() => onAnswer(answer)}>
+            <Text style={styles.answerText}>{answer}</Text>
+          </Pressable>
+        ))}
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  modal: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  question: {
+    textAlign: 'center',
+    marginTop: 24,
+    marginBottom: 16,
+    marginLeft: 16,
+    marginRight: 16,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  answerContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  answer: {
+    height: 50,
+    minWidth: 240,
+    width: 'fit-content',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderColor: '#6c5ce7',
+    borderWidth: 2,
+    borderRadius: 9,
+    margin: 8
+  },
+  answerText: {
+    fontSize: 16,
+    color: '#6c5ce7',
+  }
+})
 
 export default Modal
