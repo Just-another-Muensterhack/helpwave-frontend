4f7bf40a16deaee9fc26a255a2b9b14e87524096
Jannik Wibker <jannikwibker@gmail.com>
2022-09-23 13:18:58 +0200

    add absolutely basic modal component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Modal from './components/Modal'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal question={'idk?'} answers={[
+        'yes', 'no'
+      ]} onAnswer={console.log} />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
