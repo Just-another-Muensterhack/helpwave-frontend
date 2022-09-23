885178669d54a2bf4297578c216526027a042e37
Jannik Wibker <jannikwibker@gmail.com>
2022-09-23 16:44:46 +0200

    lint fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -8,57 +8,61 @@ type ModalProps = {
 };
 
 const Modal: React.FC<ModalProps> = ({ question, answers, onAnswer }) => {
-  return (
-    <View style={styles.modal}>
-      <Text style={styles.question}>{question}</Text>
-      <View style={styles.answerContainer}>
-        {answers.map((answer) => (
-          <Pressable style={styles.answer} key={answer} onPress={() => onAnswer(answer)}>
-            <Text style={styles.answerText}>{answer}</Text>
-          </Pressable>
-        ))}
-      </View>
-    </View>
-  )
-}
+    return (
+        <View style={styles.modal}>
+            <Text style={styles.question}>{question}</Text>
+            <View style={styles.answerContainer}>
+                {answers.map((answer) => (
+                    <Pressable
+                        style={styles.answer}
+                        key={answer}
+                        onPress={() => onAnswer(answer)}
+                    >
+                        <Text style={styles.answerText}>{answer}</Text>
+                    </Pressable>
+                ))}
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  modal: {
-    width: '100%',
-    flex: 1,
-    flexDirection: 'column',
-  },
-  question: {
-    textAlign: 'center',
-    marginTop: 24,
-    marginBottom: 16,
-    marginLeft: 16,
-    marginRight: 16,
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  answerContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  answer: {
-    height: 50,
-    minWidth: 240,
-    width: 'fit-content',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderColor: '#6c5ce7',
-    borderWidth: 2,
-    borderRadius: 9,
-    margin: 8
-  },
-  answerText: {
-    fontSize: 16,
-    color: '#6c5ce7',
-  }
-})
+    modal: {
+        width: '100%',
+        flex: 1,
+        flexDirection: 'column',
+    },
+    question: {
+        textAlign: 'center',
+        marginTop: 24,
+        marginBottom: 16,
+        marginLeft: 16,
+        marginRight: 16,
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    answerContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    answer: {
+        height: 50,
+        minWidth: 240,
+        width: 'fit-content',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderColor: '#6c5ce7',
+        borderWidth: 2,
+        borderRadius: 9,
+        margin: 8,
+    },
+    answerText: {
+        fontSize: 16,
+        color: '#6c5ce7',
+    },
+});
 
-export default Modal
+export default Modal;
