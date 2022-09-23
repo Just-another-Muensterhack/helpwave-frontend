161a85755edf7a73ba988918ea0e7da0572668ea
Max Baumann <max@bmn.dev>
2022-09-23 22:55:49 +0200

    remove auto-update of banner.png from cdn

--- .scripts/download-static.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const wget = require('node-wget-js');
-
-const CDN = "https://cdn.helpwave.de";
-const dest = path.join(__dirname, '../assets/');
-
-wget({url: `${CDN}/banner.png`, dest});
