6701c640b599d41c1f31825dcca1fca582a504c0
Max Baumann <max@bmn.dev>
2022-09-23 17:08:45 +0200

    pull banner.png and add Banner

---
+++ components/Banner.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+const Banner: React.FC = () => {
+    return (
+        <View
+            style={{
+                backgroundColor: 'black',
+                width: '100%',
+                alignItems: 'center',
+            }}
+        >
+            <Image
+                style={{ width: '50%', maxWidth: 150 }}
+                source={require('../assets/banner.png')}
+            />
+        </View>
+    );
+};
+
+export default Banner;
