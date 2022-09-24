d0eb7b70735a2f5e2f4b0d7ede6beba5a001fcc6
Max Baumann <max@bmn.dev>
2022-09-24 14:06:57 +0200

    idk

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,6 +1,3 @@
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
 
