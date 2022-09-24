2d7b0dde7d30c79109059d9a2eac4ac52afa6d17
Max Baumann <max@bmn.dev>
2022-09-24 07:59:17 +0200

    selectable

--- src/components/HWText.tsx
+++ src/components/HWText.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Text, TextProps } from 'react-native';
 
 const HWText = (props: TextProps) => {
-    return <Text style={getStyle(props)}>{props.children}</Text>;
+    return (
+        <Text style={getStyle(props)} selectable={false}>
+            {props.children}
+        </Text>
+    );
 };
 
 function getStyle(props: any) {
