5bc25976bf7ff7a47e36ee8059b9b84104bef233
Jannik Wibker <jannikwibker@gmail.com>
2022-09-24 11:19:52 +0200

    some more ui for emergency overview

--- src/components/EmergencyOverview.tsx
+++ src/components/EmergencyOverview.tsx
@@ -1,6 +1,6 @@
 import { NavigationProp } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 
 import { safeTranslate } from '../../assets/language/translationMap';
 import HWText from '../components/HWText';
@@ -25,19 +25,57 @@ const EmergencyOverviewPage: React.FC<EmergencyOverviewPageProps> = ({
     return (
         <View style={styles.container}>
             <View style={{ width: '85%', backgroundColor: ColorSecondary }}>
-                <HWText>
-                    {safeTranslate('Es sind schon', currentLanguage) + ' '}
-                    <HWText style={{ color: ColorAccentPositive }}>
-                        {2}
-                    </HWText>{' '}
-                    //TODO update
-                    {' ' +
-                        safeTranslate('Helfende auf dem Weg', currentLanguage) +
-                        '.'}{' '}
-                </HWText>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <View
+                        style={{ flex: 1, flexDirection: 'row', width: '50%' }}
+                    >
+                        <Image
+                            source={require('../../assets/profile-picture.png')}
+                            style={{
+                                resizeMode: 'contain',
+                                width: 80,
+                                height: 80,
+                                borderRadius: 40,
+                            }}
+                        />
+                        <Image
+                            source={require('../../assets/profile-picture.png')}
+                            style={{
+                                resizeMode: 'contain',
+                                width: 80,
+                                height: 80,
+                                borderRadius: 40,
+                                marginLeft: -40,
+                            }}
+                        />
+                    </View>
+                    <View style={{ width: '50%' }}>
+                        <HWText style={{ color: '#ffffff', fontSize: 16 }}>
+                            {safeTranslate('Es sind schon', currentLanguage) +
+                                ' '}
+                            <HWText style={{ color: ColorAccentPositive }}>
+                                {2}
+                            </HWText>{' '}
+                            {/* TODO update */}
+                            {' ' +
+                                safeTranslate(
+                                    'Helfende auf dem Weg',
+                                    currentLanguage,
+                                ) +
+                                '.'}{' '}
+                        </HWText>
+                    </View>
+                </View>
             </View>
-            <View style={{ width: '85%' }}>
-                <HWText>
+            <View style={{ width: '85%', marginTop: 20 }}>
+                <HWText
+                    style={{
+                        color: '#ffffff',
+                        fontSize: 24,
+                        textAlign: 'center',
+                        lineHeight: 40,
+                    }}
+                >
                     {safeTranslate('Bleibe', currentLanguage) + ' '}
                     <HWText style={{ color: ColorAccentPositive }}>
                         {safeTranslate('ruhig', currentLanguage) + ', '}
